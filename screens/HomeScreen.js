33afc4499ed747f53f7e1129d2e99913485d6467
Ende Sun <1195562121ende@gmail.com>
2024-08-13 00:13:03 +0200

    Optimizando el código antes de pasar a implementar los geocercados

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,84 +1,65 @@
 import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
-import { Marker } from "react-native-maps";
 import { fetchZones } from "./services/zones";
 import { fetchLocation } from "./services/location";
 import MapComponent from "./components/MapComponent";
-import styles from './style/styles';
+import styles from "./style/styles";
+
+const ErrorMessage = ({ message }) => (
+  <View style={styles.container}>
+    <Text style={styles.errorText}>{message}</Text>
+  </View>
+);
 
 export default function HomeScreen() {
   // #region STATES
-  // ZONE STATE
   const [zoneData, setZoneData] = useState(null);
-  const [zoneLoading, setZoneLoading] = useState(true);
-  const [zoneError, setZoneError] = useState(null);
-
-  // LOCATION STATE
   const [location, setLocation] = useState(null);
-  const [locationError, setLocationError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   // #endregion STATES
 
   // #region FETCHING
   useEffect(() => {
-    const initializedData = async () => {
+    const fetchData = async () => {
       try {
-        const zones = await fetchZones();
+        const [zones, location] = await Promise.all([fetchZones(), fetchLocation()]);
         setZoneData(zones);
-        setZoneLoading(false);
-      } catch (error) {
-        setZoneError("Error al obtener los datos");
-        setZoneLoading(false);
-      }
-    };
-
-    const fetchUserLocation = async () => {
-      try {
-        const location = await fetchLocation();
         setLocation(location);
       } catch (error) {
-        setLocationError(error.message);
+        setError("Error al obtener los datos");
+      } finally {
+        setLoading(false);
       }
     };
 
-    fetchUserLocation();
-    initializedData();
+    fetchData();
   }, []);
   // #endregion FETCHING
 
   // #region LOADING AND ERROR HANDLING
-  let text = "Waiting...";
+  if (error) {
+    return <ErrorMessage message={error} />;
+  }
 
-  if (zoneLoading) {
+  if (loading) {
     return (
       <View style={styles.container}>
-        <Text>{text}</Text>
+        <Text>Cargando...</Text>
       </View>
     );
   }
-
-  if (locationError) {
-    text = locationError;
-    return (
-      <View style={styles.container}>
-        <Text>{text}</Text>
-      </View>
-    );
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-  // #endregion
+  // #endregion LOADING AND ERROR HANDLING
 
   // #region RENDER MAP
   return (
     <View style={styles.container}>
-      <View style={styles.container}>
-        {location ? (
-          <MapComponent location={location} zoneData={zoneData} />
-        ) : (
-          <Text>Cargando mapa...</Text>
-        )}
-      </View>
+      {location && zoneData ? (
+        <MapComponent location={location} zoneData={zoneData} />
+      ) : (
+        <Text>Cargando mapa...</Text>
+      )}
     </View>
   );
-  // #endregion
+  // #endregion RENDER MAP
 }
